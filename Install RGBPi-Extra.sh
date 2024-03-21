db05413ad8f103ed0ebe6b164c0bdb36dff66c48
logantalley <47157356+logantalley@users.noreply.github.com>
2024-03-20 21:48:22 -0400

    Add files via upload

---
+++ Install RGBPi-Extra.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# GitHub repository details
+repo_owner="logantalley"
+repo_name="rgbpi-addons"
+branch="master"
+path="RGBPi-Extra"
+
+# Folder to store downloaded files
+download_folder="$SCRIPT_DIR"
+
+# URL to download the archive
+archive_url="https://github.com/$repo_owner/$repo_name/archive/$branch.tar.gz"
+
+# Create a temporary directory
+temp_dir=$(mktemp -d)
+
+# Download the archive
+curl -sL "$archive_url" | tar -xz -C "$temp_dir"
+
+# Move the directory to the specified location
+mv -v "$temp_dir/$repo_name-$branch/$path" "$download_folder"
+
+# Clean up the temporary directory
+rm -rf "$temp_dir"
+
+echo "Files downloaded successfully."
